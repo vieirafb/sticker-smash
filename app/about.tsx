9bec7561e2fb018cb53646298735e7446aa5fbf0
Luiz F. Vieira <luizfernando8966@gmail.com>
2025-01-24 15:54:09 -0300

    C2: Adiciona tela Sobre

--- app/about.tsx
+++ app/about.tsx
@@ -1,13 +1,9 @@
 import { Text, View, StyleSheet } from "react-native";
-import { Link } from "expo-router";
 
-export default function Index() {
+export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>
-        Go to About screen
-      </Link>
+      <Text style={styles.text}>About screen</Text>
     </View>
   );
 }
@@ -20,11 +16,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   text: {
-    color: '#fff',
-  },
-  button: {
-    fontSize: 20,
-    textDecorationLine: "underline",
     color: '#fff',
   },
 });
