3a5db988e8182a05e028b563b2edadae5da9024c
Luiz F. Vieira <luizfernando8966@gmail.com>
2025-01-29 11:37:12 -0300

    C2: Tab de navegação entre Home Screen e About Screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,8 +3,7 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{title: "Sticker Smash"}} />
-      <Stack.Screen name="about" options={{title: "About"}} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }}/>
     </Stack>
   );
 }
