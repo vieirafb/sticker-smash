336c8540c75eda775263fe101a876254a88e8982
Luiz F. Vieira <luizfernando8966@gmail.com>
2025-02-04 15:49:32 -0300

    C8: Implementa download da imagem na web

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,11 @@
 import { useRef, useState } from "react";
 import { captureRef } from "react-native-view-shot";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Platform } from "react-native";
 import * as ImagePicker from 'expo-image-picker';
 import { type ImageSource } from "expo-image";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import * as MediaLibrary from "expo-media-library";
+import domtoimage from 'dom-to-image';
 
 import Button from "@/components/Button";
 import CircleButton from "@/components/CircleButton";
@@ -49,18 +50,36 @@ export default function Index() {
   const onCloseModal = () => setIsModalVisible(false);
 
   const onSaveImageAsync = async () => {
-    try {
-      const localUri = await captureRef(imageRef, {
-        height: 440,
-        quality: 1
-      });
+    if (Platform.OS !== 'web') {
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1
+        });
 
-      await MediaLibrary.saveToLibraryAsync(localUri);
+        await MediaLibrary.saveToLibraryAsync(localUri);
 
-      if (localUri) alert('Saved!');
+        if (localUri) alert('Saved!');
 
-    } catch (e) {
-      console.log(e);
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        });
+
+        let link = document.createElement('a');
+        link.download = 'sticker-smash.jpeg';
+        link.href = dataUrl;
+        link.click();
+
+      } catch (e) {
+        console.log(e);
+      }
     }
   };
 
