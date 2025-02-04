4037d1179f36f5cf6b17b81d2a846d2a2bd64317
Luiz F. Vieira <luizfernando8966@gmail.com>
2025-02-04 14:06:40 -0300

    C7: Implementa screenshot da imagem com a figurinha

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { captureRef } from "react-native-view-shot";
 import { View, StyleSheet } from "react-native";
 import * as ImagePicker from 'expo-image-picker';
 import { type ImageSource } from "expo-image";
@@ -21,6 +22,7 @@ export default function Index() {
   const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
   const [pickedEmoji, setPickedEmoji] = useState<ImageSource | undefined>(undefined);
   const [status, requestPermission] = MediaLibrary.usePermissions();
+  const imageRef = useRef<View>(null);
 
   if (status === null) requestPermission();
 
@@ -47,14 +49,28 @@ export default function Index() {
   const onCloseModal = () => setIsModalVisible(false);
 
   const onSaveImageAsync = async () => {
-    /** TODO: Implementar depois */
+    try {
+      const localUri = await captureRef(imageRef, {
+        height: 440,
+        quality: 1
+      });
+
+      await MediaLibrary.saveToLibraryAsync(localUri);
+
+      if (localUri) alert('Saved!');
+
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
     <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
-        {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
+        <View ref={imageRef} collapsable={false}>
+          <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
+          {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
+        </View>
       </View>
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
